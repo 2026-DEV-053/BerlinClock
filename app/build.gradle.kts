c2b2408a5a7ff24d28161b6eb745379ec812606e
2026-DEV-053 <25216193+2026-DEV-053@users.noreply.github.com>
2026-07-05 13:02:50 +0530

    chore(deps): add hilt android test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    androidTestImplementation(libs.hilt.android.testing)
 }
